d30b5620efc6696b79207b1873f399b3a5965d83
qalkashik <mozgovoj22@gmail.com>
2026-06-05 14:01:07 +0300

    Добавлен класс ReportManager

--- labs1/Program.cs
+++ labs1/Program.cs
@@ -5,18 +5,7 @@
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
-    public class Report
-    {
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public DateTime CreationDate { get; set; }
-        public Report(string title, string content, DateTime creationDate)
-        {
-            Title = title;
-            Content = content;
-            CreationDate = creationDate;
-        }
-    }
+    
     public class ReportManager
     {
         public List<Report> Reports { get; private set; }
---
+++ labs1/ReportManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labs1
+{ 
+public class ReportManager
+{
+    public List<Report> Reports { get; private set; }
+    public ReportManager()
+    {
+        Reports = new List<Report>();
+        LoadReports();
+    }
+    public void AddReport(Report report)
+    {
+        if (report == null)
+        {
+            throw new ArgumentNullException(nameof(report));
+        }
+        Reports.Add(report);
+        SaveReports();
+    }
+    public void RemoveReport(Report report)
+    {
+        if (report == null)
+        {
+            throw new ArgumentNullException(nameof(report));
+        }
+        Reports.Remove(report);
+        SaveReports();
+    }
+    public void UpdateReport(Report report, string newTitle, string newContent)
+    {
+        if (report == null)
+        {
+            throw new ArgumentNullException(nameof(report));
+        }
+        report.Title = newTitle;
+        report.Content = newContent;
+        SaveReports();
+    }
+    private void SaveReports()
+    {
+        File.WriteAllLines("reports.txt", Reports.Select(r =>
+        $"{r.Title}|{r.Content}|{r.CreationDate.ToString("yyyy-MM-dd HH:mm:ss")}"));
+    }
+    private void LoadReports()
+    {
+        if (File.Exists("reports.txt"))
+        {
+            var lines = File.ReadAllLines("reports.txt");
+            foreach (var line in lines)
+            {
+                var parts = line.Split('|');
+                if (parts.Length == 3)
+                {
+                    DateTime creationDate;
+                    if (DateTime.TryParse(parts[2], out creationDate))
+                    {
+                        Reports.Add(new Report(parts[0], parts[1], creationDate));
+                    }
+                }
+            }
+        }
+    }
+}
