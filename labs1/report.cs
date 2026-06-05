7aeba3393f43c5ecf076c11b4087acd9ee971ed3
qalkashik <mozgovoj22@gmail.com>
2026-06-05 13:48:45 +0300

    Добавлен класс report

---
+++ labs1/report.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labs1
+{
+    public class Report
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreationDate { get; set; }
+        public Report(string title, string content, DateTime creationDate)
+        {
+            Title = title;
+            Content = content;
+            CreationDate = creationDate;
+        }
+    }
+}
